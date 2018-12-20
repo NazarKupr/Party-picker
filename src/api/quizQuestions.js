ce3042a7dd5a6cb55dd07d0de2d1442c6db01ba0
NazarKupr <KuprNazar@gmail.com>
2018-12-20 19:30:26 +0200

    Changed some styles

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,130 @@
+var quizQuestions = [
+  {
+      question: "Що з цього ви б подивились?",
+      answers: [
+          {
+              type: "Rock",
+              content: "Богемна рапсодія 2018р."
+          },
+          {
+              type: "SuperHero",
+              content: "Месники: Війна нескінченності 2018р."
+          },
+          {
+              type: "Mafia",
+              content: "Хрещений батько 1972р."
+          },
+          {
+            type: "Disco",
+            content: " Стиляги 2008р. "
+          }
+      ]
+  },
+  {
+    question: "Ваш улюблений стиль музики?",
+    answers: [
+        {
+            type: "Rock",
+            content: "Rock"
+        },
+        {
+            type: "SuperHero",
+            content: "Я меломан"
+        },
+        {
+            type: "Mafia",
+            content: "Blues"
+        },
+        {
+            type: "Disco",
+            content: "Disco"
+        }
+    ]
+},
+  {
+      question: "Ким з цих людей ви б хотіли побути день?",
+      answers: [
+          {
+              type: "Rock",
+              content: "Мерілін Менсон"
+          },
+          {
+              type: "SuperHero",
+              content: "Росомаха з фільму:Люди Х"
+          },
+          {
+              type: "Mafia",
+              content: "Аль Капоне"
+          },
+          {
+            type: "Disco",
+            content: "Баррі Ґібб з групи:Bee Gees"
+          }
+      ]
+  },
+  {
+      question: "Хто з цих виконавців вам найбільше подобається?",
+      answers: [
+          {
+              type: "Rock",
+              content: "Metallica"
+          },
+          {
+              type: "SuperHero",
+              content: "Eminem"
+          },
+          {
+              type: "Mafia",
+              content: "Чиж&Co"
+          },
+          {
+            type: "Disco",
+            content: "Abba"
+        }
+      ]
+  },
+  {
+      question: "Що з цього одягу ви б одягнули? ",
+      answers: [
+          {
+              type: "Rock",
+              content: "Косуху"
+          },
+          {
+              type: "SuperHero",
+              content: "Плащ супермена"
+          },
+          {
+              type: "Mafia",
+              content: "Костюм в клітинку"
+          },
+          {
+            type: "Disco",
+            content: "Блискучу сорочку"
+          }
+      ]
+  },
+  {
+      question: "Яку з цих країн ви б поїхали?",
+      answers: [
+          {
+              type: "Rock",
+              content: "Англія"
+          },
+          {
+              type: "SuperHero",
+              content: "США"
+          },
+          {
+              type: "Mafia",
+              content: "Італія"
+          },
+          {
+            type: "Disco",
+            content: "Німеччина"
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
